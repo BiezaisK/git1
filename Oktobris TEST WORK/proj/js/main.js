2b81fa156a210e59bf16e36cb287989791d4fe64
BiezaisK <k.biezums@gmail.com>
2020-10-31 15:10:40 +0200

    Update

--- Oktobris TEST WORK/proj/js/main.js
+++ Oktobris TEST WORK/proj/js/main.js
@@ -1,32 +1,93 @@
-document.getElementById('save-btn').addEventListener('click', function(){ 
-    const nameInput = document.getElementsByClassName('name')[0];
-    console.log(nameInput.value);
+var selectedRow = null
+
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
   
-  let users = localStorage.users;
-  
-  if (users) {
-    users = users.split(",")
-    users.push(nameInput.value);
-  
-  } else{
-    console.log("no users")
-    users = [];
-    users.push(nameInput.value);
-    console.log(users);
-    
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
   }
-  
-  localStorage.users = users.toString();
-  renderTable();
-  });
-  function renderTable(){
-    table = document.getElementsByClassName('userTable')[0];
-    table.innerHTML = '';
-  
-    const usersArray = localStorage.users.split(",");
-  
-    usersArray.forEach(function(name){
-  table.innerHTML += "<tr><td>"+ name + "</td></tr>";
-    })
-  }
-  
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["username"] = document.getElementById("username").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.username;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.email;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("city").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("username").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.empCode;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
